e7e6e5df384b2097da97086b4032add23ec3b7de
abhikanap <abhi.kanap@gmail.com>
2017-07-01 20:16:14 -0400

    This is the Initial Version of the file and is being used as a sample to load onto the Master Repository.

---
+++ Customer Data Analytics.R
@@ -0,0 +1,5 @@
+
+#This is a a R script Created for the External Factors Feature of the Project.
+#This script is for testing the basic codes and for demonstrating the code chunks. 
+# Please Do not use this script for the final coding. Please Create your own Scripts for codes.
+# Try and use the R-Markdown for the codes.
